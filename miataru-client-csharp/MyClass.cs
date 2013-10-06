a3b21e334fb65b4394dd1fed4bc73385ce4a6e95
Daniel Kirstenpfad <bietiekay@miataru.com>
2013-10-06 14:19:48 +0200

    initial commit

---
+++ miataru-client-csharp/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace miataruclientcsharp
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ miataru-client-example/miataru-client-example/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace miataruclientexample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
